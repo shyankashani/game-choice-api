7e0752fdf02216bc984958a7b46c7db023258fc3
Shyan Kashani <shyankashani1@gmail.com>
2017-10-26 19:32:01 -0700

    Updates data and query

--- db/index.js
+++ db/index.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
 
 exports.fetchGame = (players, age, duration, complexity) => {
   return new Promise ((resolve, reject) => {
-    connection.query(`SELECT * FROM games WHERE minPlayers <= ${players} AND ${players} <= maxPlayers AND age = ${age} AND duration = ${duration} AND complexity = ${complexity}`, (error, results) => {
+    connection.query(`SELECT * FROM games WHERE minPlayers <= ${players} AND ${players} <= maxPlayers AND age <= ${age} AND duration = ${duration} AND complexity = ${complexity}`, (error, results) => {
       if (error) {
         console.log(error);
       } else {
