1d7ddc8ae1df639a83febe6fff3300bbad0e6057
Shyan Kashani <shyankashani1@gmail.com>
2018-05-10 21:58:17 -0700

    Wrote endpoints for syncing and comparing that show that missing games are expansions

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ const Game = require('./models/Game');
 const Color = require('./models/Color');
 const Category = require('./models/Category');
 const Inventory = require('./models/Inventory');
+const {
+  generateGame,
+  fetchGamesByUserName,
+  fetchStatsByBggId
+} = require('./utils.js');
+
 
 const _ = require('lodash');
 
@@ -66,31 +72,100 @@ app.get('/inventory', async (req, res, next) => {
 });
 
 app.get('/sync', (req, res, next) => {
-  axios.get(`http://www.boardgamegeek.com/xmlapi2/collection?username=crabogo&stats=1`)
+  fetchGamesByUserName('victorypointcafe')
   .then(result => parse(result.data))
   .then(result => result.items.item.map(game => game.$.objectid))
   .then(result => {
-    Promise.mapSeries(result, id => {
-      return Promise.delay(4000)
+    const ids = result.slice().reverse();
+    console.log(`Number of board games: ${ids.length}`);
+    let counter = 1;
+
+    Promise.mapSeries(ids, id => {
+      return Promise.delay(6000)
       .then(async result => {
-        const game = await Game.query().select('games.*').from('games')
-          .where('bgg_id', '=', id)
-        return game.length ? Promise.resolve(id) : Promise.reject(id);
-      })
-      .then(id => {
-        console.log(`Resolved with id ${id}`)
-      })
-      .catch(id => {
-        console.log(`Rejected with id ${id}`);
-        axios.get(`http://www.boardgamegeek.com/xmlapi2/thing?id=${id.toString()}&type=boardgame&stats=1`)
-        .then(result => {
-          console.log('result', result);
-        })
-      })
-      // .then(result => axios.get(`http://www.boardgamegeek.com/xmlapi2/thing?id=${id.toString()}&type=boardgame&stats=1`))
-      // .then(result => parse(result.data))
+        const game = await Game.query().select('games.*').from('games').where('bgg_id', '=', id);
+
+        if (game.length) {
+          console.log(`${counter} Game with bgg id ${id} already exists in datbase.`);
+          _.pull(ids, id);
+          counter++;
+        }
+
+        else {
+          counter++;
+          fetchStatsByBggId(id)
+          .then(result => parse(result.data))
+          .then(async result => {
+            const newGame = generateGame(result);
+            const game = await Game.query().insertAndFetch(newGame)
+            if (game.length) {
+              console.log(`Successfully wrote ${newGame.name} to game table.`);
+              const inventory = await Inventory.query().insertAndFetch({game_id: id, organization_id: 1})
+              if (inventory.length) {
+                console.log(`Successfully added ${newGame.name} to inventory.`);
+                _.pull(ids, id);
+              }
+              else {
+                console.log(`Failed to add ${newGame.name} to database.`);
+              }
+            }
+
+            else {
+              console.log(`Failed to write ${newGame.name} to database.`);
+            }
+          })
+        }
+      }).then(result => Promise.resolve(result))
     })
   })
+})
+
+app.get('/compare', async (req, res, next) => {
+  const idsOfGamesInDatabase = await Game.query().select('games.bgg_id').from('games')
+  .then(result => result.map(game => game.bgg_id))
+
+  const idsOfGamesInVicPoint = await fetchGamesByUserName('victorypointcafe')
+  .then(result => parse(result.data))
+  .then(result => result.items.item.map(game => Number(game.$.objectid)))
+
+  console.log('idsOfGamesInDatabase.length', idsOfGamesInDatabase.length);
+  console.log('idsOfGamesInVicPoint.length', idsOfGamesInVicPoint.length);
+
+  const outstandingIds = _.difference(idsOfGamesInVicPoint, idsOfGamesInDatabase);
+
+  console.log('outstandingIds', outstandingIds);
+
+  Promise.mapSeries(outstandingIds, async id => {
+    await Promise.delay(4000)
+
+    const newGame = await fetchStatsByBggId(id)
+    .then(result => parse(result.data))
+    .then(result => console.log('result.items.$', result.items.$))
+
+    // const game = await Game.query().insertAndFetch(newGame);
+    // const inventory = await Inventory.query().insertAndFetch({
+    //   game_id: game.id,
+    //   organization_id: 1
+    // })
+    //
+    // if (game.length && inventory.length) {
+    //   console.log(`Successfully added ${game.name} to games and inventory with bgg_id of ${game.bgg_id}`);
+    // }
+    //
+    // if (game.length) {
+    //   console.log(`Successfully added ${game.name} to games but not inventory with bgg_id of ${game.bgg_id}`);
+    // }
+    //
+    // if (inventory.length) {
+    //   console.log(`Successfully added ${game.name} to inventory but not games with game_id of ${inventory.game_id}`);
+    // }
+    //
+    // else {
+    //   console.log(`Failed to add ${game.name} with bgg_id of ${game.bgg_id} to anything.`);
+    // }
+
+  });
+
 })
 
 http.listen(app.get('port'), () => {
--- utils.js
+++ utils.js
@@ -1,51 +1,36 @@
+const axios = require('axios');
+const Promise = require('bluebird');
+
 module.exports = {
-  prepare: result => {
+  generateGame: result => {
+    console.log('result.items.$', result.items.$);
     const game = result.items.item[0];
-    return {
-      bgg_id: Number(realResult.$.id),
-      name: realResult.name[0].$.value,
-      description: realResult.description[0],
-      year_published: Number(realResult.yearpublished[0].$.value),
-      image: realResult.image[0],
-      thumbnail: realResult.thumbnail[0],
-      min_players: Number(realResult.minplayers[0].$.value),
-      max_players: Number(realResult.maxplayers[0].$.value),
-      playing_time: Number(realResult.playingtime[0].$.value),
-      min_play_time: Number(realResult.minplaytime[0].$.value),
-      max_play_time: Number(realResult.maxplaytime[0].$.value),
-      min_age: Number(realResult.minage[0].$.value),
-      bgg_average_rating: Number(realResult.statistics[0].ratings[0].average[0].$.value),
-      bgg_average_weight: Number(realResult.statistics[0].ratings[0].averageweight[0].$.value)
+    console.log('game', game);
+    const gameObject = {
+      bgg_id: Number(game.$.id),
+      name: game.name[0].$.value,
+      description: game.description[0],
+      year_published: Number(game.yearpublished[0].$.value),
+      image: game.image[0],
+      thumbnail: game.thumbnail[0],
+      min_players: Number(game.minplayers[0].$.value),
+      max_players: Number(game.maxplayers[0].$.value),
+      playing_time: Number(game.playingtime[0].$.value),
+      min_play_time: Number(game.minplaytime[0].$.value),
+      max_play_time: Number(game.maxplaytime[0].$.value),
+      min_age: Number(game.minage[0].$.value),
+      bgg_average_rating: Number(game.statistics[0].ratings[0].average[0].$.value),
+      bgg_average_weight: Number(game.statistics[0].ratings[0].averageweight[0].$.value)
     }
+    console.log(`Writing game ${gameObject.name} to database.`);
+    return gameObject;
+  },
+  fetchGamesByUserName: userName => {
+    return Promise.resolve(axios.get(`http://www.boardgamegeek.com/xmlapi2/collection?username=${userName}&stats=1`));
+  },
+  fetchStatsByBggId: bggId => {
+    console.log(`Game does not exist in database. Fetching game with id ${bggId}.`);
+    const stringifiedId = bggId.toString();
+    return Promise.resolve(axios.get(`http://www.boardgamegeek.com/xmlapi2/thing?id=${stringifiedId}&type=boardgame&stats=1`));
   }
-}
-
-
-function prepareResult(result) {
-
-  let realResult = result.items.item[0];
-
-  let valuesObj = {
-    bgg_id: Number(realResult.$.id),
-    name: realResult.name[0].$.value,
-    description: realResult.description[0],
-    year_published: Number(realResult.yearpublished[0].$.value),
-    image: realResult.image[0],
-    thumbnail: realResult.thumbnail[0],
-    min_players: Number(realResult.minplayers[0].$.value),
-    max_players: Number(realResult.maxplayers[0].$.value),
-    playing_time: Number(realResult.playingtime[0].$.value),
-    min_play_time: Number(realResult.minplaytime[0].$.value),
-    max_play_time: Number(realResult.maxplaytime[0].$.value),
-    min_age: Number(realResult.minage[0].$.value),
-    bgg_average_rating: Number(realResult.statistics[0].ratings[0].average[0].$.value),
-    bgg_average_weight: Number(realResult.statistics[0].ratings[0].averageweight[0].$.value)
-  }
-
-  let preparedResult = {
-    text: 'INSERT INTO games(bgg_id, name, description, year_published, min_players, max_players, playing_time, min_play_time, max_play_time, min_age, thumbnail, image, bgg_average_weight, bgg_average_rating) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING name',
-    values: [valuesObj.bgg_id, valuesObj.name, valuesObj.description, valuesObj.year_published, valuesObj.min_players, valuesObj.max_players, valuesObj.playing_time, valuesObj.min_play_time, valuesObj.max_play_time, valuesObj.min_age, valuesObj.thumbnail, valuesObj.image, valuesObj.bgg_average_weight, valuesObj.bgg_average_rating]
-  };
-
-  return Promise.resolve(preparedResult);
 }
